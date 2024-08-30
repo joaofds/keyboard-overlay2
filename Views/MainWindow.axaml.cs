b17d035c0f809d8320db695628f5496aa220651c
Joao Silva <joao.silva@ifgoiano.edu.br>
2024-08-30 08:15:28 -0300

    qwert-asdf keys

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,21 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Input;
+using System.Collections.Generic;
+using Avalonia.Media;
+using System.IO;
 
 namespace KeyboardOverlay.Views;
 
 public partial class MainWindow : Window
 {
+
+
     public MainWindow()
     {
         InitializeComponent();
+
     }
+
+
 }
