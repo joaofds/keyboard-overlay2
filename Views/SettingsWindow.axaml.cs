9cee79b6ae2d04dee233e76ee5bc0599e73825a9
Joao Silva <jo4o.fds@gmail.com>
2024-09-03 00:19:48 -0300

    fix var names

--- Views/SettingsWindow.axaml.cs
+++ Views/SettingsWindow.axaml.cs
@@ -16,16 +16,44 @@ namespace KeyboardOverlay.Views
         public SettingsWindow()
         {
             InitializeComponent();
+
+            // Preenche cores atuais no ColorPick
+            var appSettings = SettingsService.appSettings;
+            this.SelectedFontColor.Color = Color.Parse(appSettings!.FontColor!.ToString());
+            this.SelectedBackgroundColor.Color = Color.Parse(appSettings!.BackgroundColor!.ToString());
+            this.SelectedHoverColor.Color = Color.Parse(appSettings!.HoverColor!.ToString());
+            this.SelectedBorderColor.Color = Color.Parse(appSettings!.BorderColor!.ToString());
         }
 
         // Método que pode ser chamado ao clicar no botão de salvar configurações
         private void OnSaveButtonClick(object sender, RoutedEventArgs e)
         {
+            var appSettings = SettingsService.appSettings;
+
+            // Converte cor para string removendo canal alpha
+            var SelectedFontColor = this.SelectedFontColor.Color.ToString().Remove(1, 2);
+            var SelectedHoverColor = this.SelectedHoverColor.Color.ToString().Remove(1, 2);
+            var SelectedBackgroundColor = this.SelectedBackgroundColor.Color.ToString().Remove(1, 2);
+            var SelectedBorderColor = this.SelectedBorderColor.Color.ToString().Remove(1, 2);
+
+            Debug.WriteLine(SelectedFontColor);
+            Debug.WriteLine(SelectedHoverColor);
+            Debug.WriteLine(SelectedBackgroundColor);
+            Debug.WriteLine(SelectedBorderColor);
+
+            /*
+            appSettings!.FontColor= SelectedFontColor;
+            appSettings!.HoverColor = SelectedHoverColor;
+            appSettings!.BackgroundColor = SelectedBackgroundColor;
+            appSettings!.BorderColor = SelectedBorderColor;
+            */
+
             var newSettings = new AppSettings
             {
-                FontColor = "#FFFF00", // Aqui você poderia obter esses valores de campos da UI
-                BorderColor = "#00FF00",
-                BackgroundColor = "#0000FF"
+                FontColor = this.SelectedFontColor.Color.ToString().Remove(1, 2),
+                BorderColor = this.SelectedBorderColor.Color.ToString().Remove(1, 2),
+                BackgroundColor = this.SelectedBackgroundColor.Color.ToString().Remove(1, 2),
+                HoverColor = this.SelectedHoverColor.Color.ToString().Remove(1, 2)
             };
 
             //_settingsService.SaveSettings(newSettings);
@@ -35,31 +63,6 @@ namespace KeyboardOverlay.Views
         private void OnCancelButtonClick(object? sender, RoutedEventArgs e)
         {
             this.Close();
-        }
-
-        public void Btn_Event_Test(object? source, RoutedEventArgs e)
-        {
-            var appSettings = SettingsService.appSettings;
-
-            // Converte cor para string removendo canal alpha
-            var SelectedFontColor = this.SelectedFontColor.Color.ToString().Remove(1,2);
-            var SelectedHoverColor = this.SelectedHoverColor.Color.ToString().Remove(1,2);
-            var SelectedBackColor = this.SelectedBackColor.Color.ToString().Remove(1,2);
-            var SelectedBorderColor = this.SelectedBorderColor.Color.ToString().Remove(1,2);
-
-            Debug.WriteLine(SelectedFontColor);
-            Debug.WriteLine(SelectedHoverColor);
-            Debug.WriteLine(SelectedBackColor);
-            Debug.WriteLine(SelectedBorderColor);
-
-            /*
-            appSettings!.FontColor= SelectedFontColor;
-            appSettings!.HoverColor = SelectedHoverColor;
-            appSettings!.BackgroundColor = SelectedBackColor;
-            appSettings!.BorderColor = SelectedBorderColor;
-            */
-            
-
         }
     }
 }
